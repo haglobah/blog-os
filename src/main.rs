303f221d1f2ffb9bab8a3eb7f2f8e30254f0426b
Beat Hagenlocher <bah@posteo.de>
2023-09-20 21:45:40 +0200

    Add output test, fix nasty off-by-one bug
    Tested-by: Beat Hagenlocher <bah@posteo.de>

--- src/main.rs
+++ src/main.rs
@@ -45,9 +45,7 @@ pub trait Testable {
     fn run(&self) -> ();
 }
 
-impl<T> Testable for T where
-    T: Fn();
-{
+impl<T> Testable for T where T: Fn() {
     fn run(&self) {
         serial_print!("{}...\t", core::any::type_name::<T>());
         self();
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -77,7 +77,7 @@ impl Writer {
 
     fn new_line(&mut self) {
         for row in 1..BUFFER_HEIGHT {
-            for col in 1..BUFFER_WIDTH {
+            for col in 0..BUFFER_WIDTH {
                 let character = self.buffer.chars[row][col].read();
                 self.buffer.chars[row - 1][col].write(character);
             }
@@ -144,4 +144,26 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
